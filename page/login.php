6b5deed31bbe66ef8b69e83d642f62e7f4b5b7fc
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-09-14 15:21:01 +0300

    up2

--- page/login.php
+++ page/login.php
@@ -5,9 +5,14 @@
 	//ERRORid
 	$email_error= "";
 	$password_error="";
+	$name_error="";
+	$new_email_error="";
+	$new_password_error="";
 	
 	//väärtustega muutujad
 	$email="";
+	$password="";
+	$name="";
      
 	//kontrolli ainult siis kui kasutaja vajutab login nuppu.
 	if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -20,25 +25,55 @@
 				} else {
 					
 					//annan väärtuse
-					$email=$_POST["email"];
+					$email=test_input($_POST["email"]);
 				}
 		
 		
-			if($_SERVER["REQUEST_METHOD"] == "POST"){
-		 
+				
 				//kontrollime kasutaja parooli, et see pole tühi.
 					if(empty($_POST ["password"])) {
 						$password_error= "See on kohustuslik väli";
 					} else {
-						//parool ei ole tühi, kontrollime pikkust
-						if(strlen($_POST ["password"]) < 6){
-							$password_error="Peab olema vähemalt 6 sümbolit pikk";
-						}
+						$password=test_input($_POST["password"]);
+					}
+					
+					if($password_error == "" && $email_error == ""){
+						//erroreid ei olnud
+						echo "Kontollin  ".$email." ".$password;
+					}
+		}
+	}
+			if($_SERVER["REQUEST_METHOD"] == "POST"){
+				if(isset($_POST["create"])){				
+				//loomisel emaili kontroll
+					if(empty($_POST ["new_email"])) {
+						$new_email_error= "See on kohustuslik väli";
+					} else {
+					}
+				
+				//loomisel parooli kontroll
+					if(empty($_POST ["new_password"])) {
+						$new_password_error= "See on kohustuslik väli";
+					} else {
+				//parool ei ole tühi, kontrollime pikkust
+					if(strlen($_POST ["password"]) < 6){
+						$new_password_error="Parool peab olema vähemalt 6 sümbolit pikk";
+					}
+					}
+				
+				//kasutajanime kontroll 
+					if(empty($_POST ["name"])) {
+						$name_error= "Palun sisesta oma kasutajanimi";
+					} else {
 					}
 				}
 			}
+		function test_input($data) {
+		  $data = trim($data);
+		  $data = stripslashes($data);
+		  $data = htmlspecialchars($data);
+		  return $data;
 		}
-		
 ?>
 
 <?php
@@ -53,13 +88,19 @@
 
 
 		<h2>Login</h2>
-		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 			<input name="email" type="text" placeholder="E-mail" value="<?php echo $email; ?>">* <?php echo $email_error; ?> <br><br>
 			<input name="password" type="password" placeholder="password">* <?php echo $password_error; ?><br><br>
 		<input name="login" type="submit" value="Log in">
 		</form>
 		
 		<h2>Create user</h2>
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+			<input name="email" type="text" placeholder="E-mail" value="<?php echo $email; ?>">* <?php echo $new_email_error; ?> <br><br>
+			<input name="password" type="password" placeholder="password">* <?php echo $new_password_error; ?><br><br>
+			<input name="name" type="text" placeholder="Kasutajanimi" >* <?php echo $name_error; ?> <br><br>
+			<input name="create" type ="submit" value="Create user">
+		</form>
 
 		
 		
